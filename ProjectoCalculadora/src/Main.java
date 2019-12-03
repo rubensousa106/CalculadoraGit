40c6308673396995c4f4ca14f9ccb99ca6dd7c05
Ruben Sousa <rubensousa106@gmail.com>
2019-12-03 21:08:25 +0000

    criei menu

--- ProjectoCalculadora/src/Main.java
+++ ProjectoCalculadora/src/Main.java
@@ -1,20 +1,26 @@
 import java.util.Scanner;
 
 public class Main {
-	static Scanner sc = new Scanner (System.in);
+	static Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		System.out.println("Projecto Calculadora");
-		
+
 		System.out.println("Insira o primeiro numero :");
 		int a = sc.nextInt();
-		
+
 		System.out.println("Insira o segundo numero :");
 		int b = sc.nextInt();
-		System.out.println("Valor a : "+ a + " Valor b : " + b);
-		System.out.println("#");
-	
-    }
+		System.out.println("Valor a : " + a + " Valor b : " + b);
+		
+		System.out.println("**Escolha o tipo de operação _: **");
+		System.out.println("1 - Adiçao");
+		System.out.println("2 - Subtração");
+		System.out.println("3 - Multiplicação");
+		System.out.println("4 - Divisão");
+		
+		
+		
+		
 	}
-
-	
-
+}
