9112a4f2deff3853f01754c332f613cfd5c60647
Ruben Sousa <rubensousa106@gmail.com>
2019-12-03 21:03:52 +0000

    primeiras alteraçoes

--- ProjectoCalculadora/src/Main.java
+++ ProjectoCalculadora/src/Main.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
 
 public class Main {
-
+	static Scanner sc = new Scanner (System.in);
 	public static void main(String[] args) {
 		System.out.println("Projecto Calculadora");
-
+		
+		System.out.println("Insira o primeiro numero :");
+		int a = sc.nextInt();
+		
+		System.out.println("Insira o segundo numero :");
+		int b = sc.nextInt();
+		System.out.println("Valor a : "+ a + " Valor b : " + b);
+		
+	
+    }
 	}
 
-}
+	
+
