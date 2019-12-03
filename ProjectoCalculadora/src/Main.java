6a2e549a493eb0573c5f5c9bb9ded763e972dbe8
Ruben Sousa <rubensousa106@gmail.com>
2019-12-03 21:33:29 +0000

    adicinar classe multiplicar ao menu

--- ProjectoCalculadora/src/Main.java
+++ ProjectoCalculadora/src/Main.java
@@ -12,35 +12,30 @@ public class Main {
 		System.out.println("Insira o segundo numero :");
 		int b = sc.nextInt();
 		System.out.println("Valor a : " + a + " Valor b : " + b);
-		
+
 		System.out.println("**Escolha o tipo de operação : **");
 		System.out.println("1 - Adiçao");
 		System.out.println("2 - Subtração");
 		System.out.println("3 - Multiplicação");
 		System.out.println("4 - Divisão");
-		int op=0;;
-		switch (op) {
-		case 1:
-			System.out.println(ClasseSoma.soma(a, b));
-	       break;
-       
-        
-                
-        default: break;
-    }
-		
+		int op = 0;
+		;
 		do {
-            op = sc.nextInt();
-            switch (op) {
-            case 1:
-    			System.out.println(ClasseSoma.soma(a, b));
-    	       break;
-            case 2:
-    			System.out.println(ClasseSubtracao.subtrair(a, b));
-    	       break;
-            } 
-        } while (op<4); // end of loop
-		
-		
+			op = sc.nextInt();
+			switch (op) {
+			case 1:
+				System.out.println(ClasseSoma.soma(a, b));
+				break;
+			case 2:
+				System.out.println(ClasseSubtracao.subtrair(a, b));
+				break;
+
+			case 3:
+				System.out.println(ClasseMultiplicacao.multiplicar(a, b));
+				break;
+			}
+
+		} while (op < 4); // end of loop
+
 	}
 }
